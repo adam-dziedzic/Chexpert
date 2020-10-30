1b7fecf09e86d57d8aba5eae13cde15b5712a0f5
adam-dziedzic <adam.dziedzi@gmail.com>
2020-10-29 23:32:17 -0400

    train classifiers per disease

--- bin/train_single_disease.sh
+++ bin/train_single_disease.sh
@@ -13,6 +13,12 @@ train_single_disease_2020-10-29-16-17-51-426497739.txt
 [1] 206460
 (venv-tf-py3) dockuser@nic3:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
 train_single_disease_2020-10-29-17-47-53-334858213.txt
+207391
+(venv-tf-py3) dockuser@nic3:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-17-52-17-663191952.txt
+[1] 208382
+(venv-tf-py3) dockuser@nic3:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-17-54-54-519854293.txt
 
 
 # Edema
@@ -24,5 +30,56 @@ nohup python train_single_disease.py \
 --verbose False \
 >> train_${DATASET}_${timestamp}.txt 2>&1 &
 echo train_${DATASET}_${timestamp}.txt
+[1] 220134
+(chexpert) nicolas@equus:~/code/Chexpert-pycharm/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-16-16-40-456427931.txt
+
+# Consolidation
+timestamp=$(date +%Y-%m-%d-%H-%M-%S-%N)
+DATASET='single_disease'
+CUDA_VISIBLE_DEVICES=0,1,2,3
+PYTHONPATH=../
+nohup python train_single_disease.py \
+--verbose False \
+>> train_${DATASET}_${timestamp}.txt 2>&1 &
+echo train_${DATASET}_${timestamp}.txt
+[1] 21385
+dockuser@nic2:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-17-51-07-142710798.txt
+[1] 21492
+dockuser@nic2:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-17-54-06-387388986.txt
+[1] 21583
+dockuser@nic2:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-18-00-21-721477172.txt
+
+
+# Atelectasis
+timestamp=$(date +%Y-%m-%d-%H-%M-%S-%N)
+DATASET='single_disease'
+CUDA_VISIBLE_DEVICES=0,1,2,3
+PYTHONPATH=../
+nohup python train_single_disease.py \
+--verbose False \
+>> train_${DATASET}_${timestamp}.txt 2>&1 &
+echo train_${DATASET}_${timestamp}.txt
+[1] 297069
+(chexpert) nicolas@equus:~/code/Chexpert-pycharm/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-23-20-51-960340680.txt
+
+
+# Pleural Effusion
+timestamp=$(date +%Y-%m-%d-%H-%M-%S-%N)
+DATASET='single_disease'
+CUDA_VISIBLE_DEVICES=0,1,2,3
+PYTHONPATH=../
+nohup python train_single_disease.py \
+--verbose False \
+>> train_${DATASET}_${timestamp}.txt 2>&1 &
+echo train_${DATASET}_${timestamp}.txt
+[1] 230303
+(venv-tf-py3) dockuser@nic3:~/code/Chexpert/bin$ echo train_${DATASET}_${timestamp}.txt
+train_single_disease_2020-10-29-23-22-26-089611049.txt
+
 
 
